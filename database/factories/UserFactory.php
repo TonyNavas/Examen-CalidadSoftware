05728e53cbb0b0df87037a2ab05f2964149a0f02
TonyNavas <tonynavas777@gmail.com>
2021-11-17 15:58:31 -0600

    Agregue el factory de users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,8 +2,9 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 class UserFactory extends Factory
 {
